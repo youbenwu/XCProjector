925a6285263a7bc97a48c0a4ddbbc88291c7357d
youbenwu <youbenwu@live.com>
2023-09-13 13:57:19 +0800

    首页UI

--- app/src/main/java/com/outmao/xcprojector/SplashActivity.java
+++ app/src/main/java/com/outmao/xcprojector/SplashActivity.java
@@ -3,12 +3,19 @@ package com.outmao.xcprojector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageInfo;
+import android.content.pm.ResolveInfo;
 import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Toast;
 import com.outmao.xcprojector.api.HttpApiService;
@@ -19,12 +26,16 @@ import com.outmao.xcprojector.network.RxSubscriber;
 import com.outmao.xcprojector.network.YYResponseData;
 import com.outmao.xcprojector.util.SharepreferencesUtils;
 
+import java.util.List;
+
 
 public class SplashActivity extends AppCompatActivity {
 
     private boolean first=true;
 
     private ActivitySplashBinding binding;
+
+    private boolean error=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +48,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(!first){
-                    checkState();
+                    go();
                 }
             }
         });
@@ -46,11 +57,12 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                checkState();
+                go();
                 first=false;
             }
-        }, 100);
+        }, 1500);
 
+        registerWifiStateReceiver();
 
     }
 
@@ -58,6 +70,19 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
+    }
+
+    private void go(){
+        if(SharepreferencesUtils.getShareInstance().getString(AppConfig.NEW_ACTIVE)==null){
+            goActivate();
+            return;
+        }
+        if(!isNetwork()){
+            Toast.makeText(getBaseContext(), "无网络，请先联接WIFY", Toast.LENGTH_LONG).show();
+            error=true;
+            return;
+        }
+        checkState();
     }
 
     //去激活设备
@@ -74,6 +99,13 @@ public class SplashActivity extends AppCompatActivity {
         //finish();
     }
 
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getKeyCode() == KeyEvent.KEYCODE_SETTINGS) {
+            goSetting();
+            return true;
+        }
+        return super.onKeyDown(keyCode,event);
+    }
 
     private boolean isNetwork(){
         try {
@@ -87,25 +119,13 @@ public class SplashActivity extends AppCompatActivity {
 
    //检测设备是否激活
    private void checkState() {
-        if(!isNetwork()){
-            if(SharepreferencesUtils.getShareInstance().getString(AppConfig.NEW_ACTIVE)!=null){
-                goMain();
-            }else{
-                goActivate();
-            }
-            return;
-        }
        HttpApiService.getInstance().account_status()
                .subscribe(new RxSubscriber<YYResponseData<AccountStatusData>>() {
                    @Override
                    public void onFail(YYResponseData<AccountStatusData> responseData) {
                        super.onFail(responseData);
                        Toast.makeText(getBaseContext(), responseData.getMessage(), Toast.LENGTH_LONG).show();
-                       if(SharepreferencesUtils.getShareInstance().getString(AppConfig.NEW_ACTIVE)!=null){
-                           goMain();
-                       }else{
-                           goActivate();
-                       }
+                       error=true;
                    }
 
                    @Override
@@ -118,9 +138,81 @@ public class SplashActivity extends AppCompatActivity {
                        }else{
                            goActivate();
                        }
+                       error=false;
                    }
                });
    }
+
+
+    private void registerWifiStateReceiver(){
+        // 创建Wifi状态变化的IntentFilter
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        // 注册BroadcastReceiver
+        registerReceiver(wifiStateReceiver, intentFilter);
+    }
+
+    private BroadcastReceiver wifiStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int wifiState = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN);
+            switch (wifiState) {
+                case WifiManager.WIFI_STATE_ENABLED:
+                    // Wifi已启用
+                    if(error==true){
+                        error=false;
+                        go();
+                    }
+                    break;
+                case WifiManager.WIFI_STATE_DISABLED:
+                    // Wifi已禁用
+                    break;
+                case WifiManager.WIFI_STATE_UNKNOWN:
+                    // Wifi状态未知
+
+                    break;
+            }
+        }
+    };
+
+   private void goSetting(){
+       try{
+           String actionName = "com.htc.htcpublicsettings";
+           // 包名不是Null 的时候跳转
+           if(actionName != null) {
+               PackageInfo pi = getPackageManager().getPackageInfo(actionName, 0);
+
+               Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+               resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+               resolveIntent.setPackage(pi.packageName);
+
+               List<ResolveInfo> apps = getPackageManager().queryIntentActivities(resolveIntent, 0);
+
+               ResolveInfo ri = apps.iterator().next();
+               if (ri != null ) {
+                   String packageName = ri.activityInfo.packageName;
+                   String className = ri.activityInfo.name;
+
+                   Intent intent = new Intent(Intent.ACTION_MAIN);
+                   intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                   ComponentName cn = new ComponentName(packageName, className);
+
+                   intent.setComponent(cn);
+                   startActivity(intent);
+               } else {
+                   // 包名是Null的时候,提示用户下载
+
+               }
+
+
+           }
+       }catch(Exception e) {
+           e.printStackTrace();
+           // Toast.makeText(getContext(), "暂无未支持该应用", Toast.LENGTH_LONG).show();
+           Log.d("OtherApplication Error: ", "otherApplication.getPackageName()");
+       }
+   }
+
 
 
 
